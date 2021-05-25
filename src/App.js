4e7057d7f954b9cf70e0f172e82600fb8252b5ce
Izzul Syahmi Rizal <izzulsyahmi210@gmail.com>
2021-05-25 16:52:40 -0700

    Implemented React Router

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
 import React, {Fragment, useEffect} from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import { Route, Switch, Link, withRouter } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
+/** Components **/
 import Portfolio from './components/layout/portfolio/Portfolio';
 import About from './components/layout/about/About';
-import Artworks from './components/layout/artworks/Artworks';
 import Projects from './components/layout/projects/Projects';
+import Blog from './components/layout/blog/Blog';
+import Artworks from './components/layout/artworks/Artworks';
+
+/** CSS **/
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Navbar.css';
+
+/** PNG files  **/
+import webLogo from './assets/web_logo.png';
 
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
+
       <Navbar expand="lg" collapseOnSelect>
         <Navbar.Brand>
-          Logo
+          <Link to="/"><img src={webLogo}></img></Link>
         </Navbar.Brand>
 
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav>
-            <NavDropdown>
-              <NavDropdown.Item>Nav 1</NavDropdown.Item>
-              <NavDropdown.Item>Nav 2</NavDropdown.Item>
-              <NavDropdown.Item>Nav 3</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav.Link>About</Nav.Link>
-          <Nav.Link>Projects</Nav.Link>
-          <Nav.Link>Artworks</Nav.Link>
+            <Link to="/about">About</Link>
+          </Nav> 
+          <Nav>
+            <Link to="/projects">Projects</Link>
+          </Nav> 
+          <Nav>
+            <Link to="/blog">Blog</Link>
+          </Nav> 
+          <Nav>
+            <Link to="/artworks">Artworks</Link>
+          </Nav> 
         </Navbar.Collapse>
-
       </Navbar>
 
-      
+      <Route exact path="/" component={Portfolio} />
+      <Switch>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/blog" component={Blog} />
+        <Route exact path="/artworks" component={Artworks} />
+      </Switch>
 
-    </div>
+    </Fragment>
   );
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/layout/blog/Blog.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class Blog extends Component{
+    render(){
+        return(
+            <Fragment>
+                <div>
+                    <p>This is in Blog.js</p>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Blog;
