b8ce04f1111744347678211f4f190127e9ee4c60
Izzul Syahmi Rizal <izzulsyahmi210@gmail.com>
2021-05-25 14:46:56 -0700

    First commit

---
+++ src/components/layout/portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+
+class Portfolio extends Component{
+    render(){
+        return(
+            <Fragment>
+                <div>
+                    <p>This is in Portfolio.js</p>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Portfolio;
