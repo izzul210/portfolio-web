f1c3047a65afaf5e2c66527004ee21d7da8d1176
Izzul Syahmi Rizal <izzulsyahmi210@gmail.com>
2021-05-27 16:01:53 -0700

    Implemented iTyped library

--- src/components/layout/home/Header.jsx
+++ src/components/layout/home/Header.jsx
@@ -5,6 +5,11 @@ import headerImage from '../../../assets/default_header.png';
 
 
 class Header extends Component{
+    componentDidMount(){
+        const myElement = document.querySelector('#myElement')
+        init(myElement, { showCursor: true, strings: ['Web Development', 'Electrical Engineering' ] })
+      }
+
     render(){
         return(
             <Fragment>
@@ -12,12 +17,12 @@ class Header extends Component{
                     <div className="header_text">
                         <div className="header_title">
                             <h1>Hi, I'm Izzul Syahmi</h1>
-                            <h2>Web Development</h2>
+                            <h2><span id="myElement"></span></h2>
                         </div>
                         <div className="header_about">
                             <p>I'm a 24 years old recent Electrical Engineering graduate from UBC, Vancouver.
                                 This website showcase all of my projects and portfolios on <b>Web Development</b>, 
-                                <b>Engineering</b>, and <b> Machine Learning</b>. 
+                                <b> Engineering</b>, and <b> Machine Learning</b>. 
                             </p>
                         </div>
                         <div className="header_button">
