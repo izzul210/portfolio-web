81dbc1d769435b2b4fa97838ebaeafcdd6866e97
Izzul Syahmi Rizal <izzulsyahmi210@gmail.com>
2021-05-28 15:33:55 -0700

    Portfolio for Web Dev

--- src/components/layout/home/Portfolio.jsx
+++ src/components/layout/home/Portfolio.jsx
@@ -3,6 +3,8 @@ import React, { Component, Fragment } from 'react';
 import './Portfolio.scss';
 
 import project1img from '../../../assets/socialnetwork1.png';
+import project2img from '../../../assets/racingcar.png';
+import project3img from '../../../assets/socialmedia.png';
 
 class Portfolio extends Component{
     render(){
@@ -18,12 +20,11 @@ class Portfolio extends Component{
                         </div>
                         <div className="webdev_portfolios">
                             <div className="webdev_1">
-                                <div className="webdev_project1">
-                                    <div className="project1_des">
-                                        <h3>Social Media Web App</h3>
+                                <div className="webdev_project">
+                                    <div className="project_des">
+                                        <h3>Social Network Web App</h3>
                                         <p>Created a system that reads sound samples from an off-chip flash memory 
-                                           and store them to the on-chip memory using a state machine. The samples 
-                                           are transferred to the audio core as an output by designing audio CODEC</p>
+                                           and store them to the on-chip memory using a state machine.</p>
                                         <div className="project_tags">
                                             <p>ReactJS</p>
                                             <p>Redux</p>
@@ -35,29 +36,53 @@ class Portfolio extends Component{
                                             <button className="more_button">Learn More</button>
                                         </div>
                                     </div>
-                                    <div className="project1_img">
+                                    <div className="project_img">
                                         <img src={project1img} alt=""></img>
                                     </div>
                                 </div>
                             </div>
                             <div className="webdev_2">
-                                <div className="webdev_project2">
-                                    <div className="project2_img">
-                                        
+                                <div className="webdev_project">
+                                    <div className="project_img">
+                                        <img src={project2img} alt=""></img>
                                     </div>
-                                    <div className="project2_des">
-
+                                    <div className="project_des">
+                                        <h3>Neuroevolution Racing Cars</h3>
+                                        <p> The samples are transferred to the audio core as an output by designing audio CODEC</p>
+                                        <div className="project_tags">
+                                            <p>TensorflowJS</p>
+                                            <p>Machine Learning</p>
+                                            <p>p5.js</p>
+                                            <p>HTML Canvas</p>
+                                            <p>Javascript</p>
+                                        </div>
+                                        <div className="project_buttons">
+                                            <button className="web_button">View Site</button>
+                                            <button className="more_button">Learn More</button>
+                                        </div>
                                     </div>
                                     
                                 </div>
                             </div>
                             <div className="webdev_3">
-                                <div className="webdev_project3">
-                                    <div className="project3_des">
-
+                                <div className="webdev_project">
+                                    <div className="project_des">
+                                        <h3>Social Media Web App</h3>
+                                        <p>Created a system that reads sound samples from an off-chip flash memory 
+                                           and store them to the on-chip memory using a state machine.</p>
+                                        <div className="project_tags">
+                                            <p>ReactJS</p>
+                                            <p>Redux</p>
+                                            <p>Firebase</p>
+                                            <p>MaterialUI</p>
+                                        </div>
+                                        <div className="project_buttons">
+                                            <button className="web_button">View Site</button>
+                                            <button className="more_button">Learn More</button>
+                                        </div>
                                     </div>
-                                    <div className="project3_img">
-                                        
+                                    <div className="project_img">
+                                        <img src={project3img} alt=""></img>
                                     </div>
                                 </div>
 
